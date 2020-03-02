7e8ae8663cddc2eb63686cd5e6f19cfa8a8984f3
Carles Giménez <Carles.Gimenez@dnvgl.com>
2020-03-02 12:57:26 +0100

    Simplifyed Regex matching

--- TextLoaderFactory/StudentFactory.cs
+++ TextLoaderFactory/StudentFactory.cs
@@ -7,31 +7,24 @@ namespace TextLoaderFactory
     {
         public override Student ReadToObject(string text)
         {
-			var match = Regex.Match(text, @"([^,]+),(M|m|F|f),(\d+),([^,]+),(\d+)");
+			var match = Regex.Match(text, @"(([^,]+),)?(M|m|F|f),(\d+),([^,]+),(\d+)");
 
 			if (match.Success)
 			{
-				return new Student(match.Groups[1].Value,
-					Convert.ToChar(match.Groups[2].Value),
-					Convert.ToInt32(match.Groups[3].Value),
-					match.Groups[4].Value,
-					Convert.ToInt32(match.Groups[5].Value));
+				return !string.IsNullOrEmpty(match.Groups[1].Value)
+					? new Student(match.Groups[2].Value,
+						Convert.ToChar(match.Groups[3].Value),
+						Convert.ToInt32(match.Groups[4].Value),
+						match.Groups[5].Value,
+						Convert.ToInt32(match.Groups[6].Value))
+					: new Student(Convert.ToChar(match.Groups[3].Value),
+						Convert.ToInt32(match.Groups[4].Value),
+						match.Groups[5].Value,
+						Convert.ToInt32(match.Groups[6].Value));
 			}
 			else
 			{
-				match = Regex.Match(text, @"(M|m|F|f),(\d+),([^,]+),(\d+)");
-
-				if (match.Success)
-				{
-					return new Student(Convert.ToChar(match.Groups[1].Value),
-						Convert.ToInt32(match.Groups[2].Value),
-						match.Groups[3].Value,
-						Convert.ToInt32(match.Groups[4].Value));
-				}
-				else
-				{
-					throw new ArgumentException("The student string is not properly formatted");
-				}
+				throw new ArgumentException("The student string is not properly formatted");
 			}
 		}
     }
